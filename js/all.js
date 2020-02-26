8cf3e59b79b9bfdfb1c0ddbe068391999ae80ef7
qaz54783305 <z5778223609969@gmail.com>
2020-02-26 21:48:26 +0800

    整理css

--- js/all.js
+++ js/all.js
@@ -28,8 +28,30 @@ $('.start').click(function(event) {
 });
 
 
+$('.close').click(function(event) {
+	event.preventDefault();
+$('.box3').slideToggle(1000);
+});
 
 
+
+ $(".box4").hover(function() {
+ 	    $(this).css("border", "10px solid #ff0000");  //mouseenter產生邊框
+        $(this).css("color", "#ff0000");  //mouseenter時改變字的顏色
+        }, function(){
+        $(this).css("border", "none");  //mouseleave時去除邊框
+        $(this).css("color", "#000");  //mouseleave時字的顏色改成黑色字的顏色改成黑色
+ 
+
+
+ });
+ 
+$('.dropdown').click(function(event) {
+    /* Act on the event */
+    event.preventDefault();
+    $('.dropdown').toggleClass('active');
+    $('.dropdown-open').slideToggle();
+  });
 
 });	
 
